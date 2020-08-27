fdc241b9e4e3f946f9b24d1d39198cd34b4ee6a7
Lucas <lixinnevergiveup@gmail.com>
2020-08-27 23:10:46 +0800

    sqlite database configure.

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/astaxie/beego v1.12.2
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible
 	github.com/smartystreets/goconvey v1.6.4
-	github.com/unknwon/com v1.0.1 // indirect
+	github.com/unknwon/com v1.0.1
 )
--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
-	_ "SimpleBlog/routers"
+	"SimpleBlog/controllers"
+	"SimpleBlog/models"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
 )
 
+func init() {
+	models.RegisterDB()
+}
+
 func main() {
+	orm.Debug = true
+	orm.RunSyncdb("default", false, true)
+
+	beego.Router("/", &controllers.MainController{})
 	beego.Run()
 }
